daa3646e3a1a4cbeff815ddcd34942c8576627e1
Wan Aqim <wanaqim@gmail.com>
2024-02-29 11:24:12 +0800

    set to async function and change base url

--- src/API Services/productServices.js
+++ src/API Services/productServices.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3001'; 
+const API_BASE_URL = '/api/inventory'; 
 
-function getProducts() {
+async function getProducts() {
     return axios.get(`${API_BASE_URL}/inventories`);
 }
   
-  function createProduct(productData) {
-    return axios.post(`${API_BASE_URL}/add-inventory`, productData);
+async function createProduct(productData) {
+  return axios.post(`${API_BASE_URL}/add-inventory`, productData);
 }
   
-  function updateProduct(id, updateData) {
-    return axios.put(`${API_BASE_URL}/update-inventory/${id}`, updateData);
+async function updateProduct(id, updateData) {
+  return axios.put(`${API_BASE_URL}/update-inventory/${id}`, updateData);
 }
   
-  function deleteProduct(id) {
-    return axios.delete(`${API_BASE_URL}/delete-inventory/${id}`);
+async function deleteProduct(id) {
+  return axios.delete(`${API_BASE_URL}/delete-inventory/${id}`);
 }
   
 export { getProducts, createProduct, updateProduct, deleteProduct };
