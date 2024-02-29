10f5bb04bee2c0f537340909edf4a2a54c692010
Wan Aqim <wanaqim@gmail.com>
2024-02-29 11:23:09 +0800

    add new route for add product

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,15 +1,14 @@
-import productPage from '../../Components/ProductsTable'
 import { Routes, Route } from 'react-router-dom'
 import ProductPage from '../ProductPage/ProductPage'
-
+import CreateProductPage from '../ProductPage/CreateProduct'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/products' element={ProductPage} exact />
-
+        <Route path='/' element={<ProductPage/>} exact/>
+        <Route path="/add-product" element={<CreateProductPage />} />
       </Routes>
     </>
   )
