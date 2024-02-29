74bfa66fcf92405ad0d05edcb694fb58f13f18c1
Wan Aqim <wanaqim@gmail.com>
2024-02-29 17:07:19 +0800

    add edit product page

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import ProductPage from '../ProductPage/ProductPage'
 import CreateProductPage from '../ProductPage/CreateProduct'
 import ProductInfoPage from '../ProductPage/ProductInfo'
+import EditProductPage from '../ProductPage/EditProduct'
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
         <Route path='/' element={<ProductPage/>} exact/>
         <Route path="/add-product" element={<CreateProductPage />} />
         <Route path="/product-info/:productID" element={<ProductInfoPage />} />
+        <Route path="/edit-product/:productID" element={<EditProductPage />} />
+
       </Routes>
     </>
   )
