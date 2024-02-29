18bcf40f3720cc7f879cab008df0950004fac18e
Wan Aqim <wanaqim@gmail.com>
2024-02-29 09:44:48 +0800

    change import path

--- server.cjs
+++ server.cjs
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const app = express();
 
 require('dotenv').config();
-require('./config/database.cjs');
+require('./backend/config/database.cjs');
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use('api/inventory', require('./backend/routes/products.cjs'));
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 3001;
