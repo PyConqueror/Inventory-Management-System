e16431356ad5cddf2f0318190e01e8a872cbf2d5
Wan Aqim <wanaqim@gmail.com>
2024-02-29 11:24:42 +0800

    fix data not displaying properly

--- src/Components/ProductsTable.jsx
+++ src/Components/ProductsTable.jsx
@@ -2,42 +2,50 @@ import { useState, useEffect } from 'react';
 import * as productServices from '../API Services/productServices';
 
 function ProductTable() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(null);
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
 async function fetchProducts() {
-const data = await productServices.getProducts();
-setProducts(data)
+const response = await productServices.getProducts();
+setProducts(response.data)
 }
 
-return (
-<div>
-    <h2>Products Inventory</h2>
-    <table>
-    <thead>
-        <tr>
-        <th>Item Name</th>
-        <th>Description</th>
-        <th>Stock Quantity</th>
-        <th>Supplier ID</th>
-        </tr>
-    </thead>
-    <tbody>
-        {products.map(product => (
-        <tr key={product._id}>
-            <td>{product.itemName}</td>
-            <td>{product.description || 'N/A'}</td>
-            <td>{product.stockQuantity}</td>
-            <td>{product.supplier || 'N/A'}</td>
-        </tr>
-        ))}
-    </tbody>
-    </table>
-</div>
-);
+if (products === null) {
+  return <div>Loading...</div>;
 }
+
+else return (
+  
+  <div>
+    <h2>Product Inventory</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Item Name</th>
+          <th>Description</th>
+          <th>Stock Quantity</th>
+          <th>Supplier ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map(product => (
+          <tr key={product._id}>
+            <td>{product.itemName}</td>
+            <td>{product.description}</td>
+            <td>{product.stockQuantity}</td>
+            <td>{product.supplier}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
+
+
+}
+
 
 export default ProductTable;
