da4e4505cc9de43e8f7db3b32563e73f296c3792
Wan Aqim <wanaqim@gmail.com>
2024-03-01 11:20:05 +0800

    add api route

--- server.cjs
+++ server.cjs
@@ -8,9 +8,11 @@ require('./backend/config/database.cjs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(require('./backend/config/checkToken.cjs'))
 
 app.use('/api/inventory', require('./backend/routes/products.cjs'));
 app.use('/api/supplier', require('./backend/routes/suppliers.cjs'));
+app.use('/api/users', require('./backend/routes/users.cjs'));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
