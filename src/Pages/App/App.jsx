e94592791bb478d71ab7fe91d0becabbb93780f2
Wan Aqim <wanaqim@gmail.com>
2024-03-01 08:08:50 +0800

    add navbar to App

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -3,17 +3,18 @@ import ProductPage from '../ProductPage/ProductPage'
 import CreateProductPage from '../ProductPage/CreateProduct'
 import ProductInfoPage from '../ProductPage/ProductInfo'
 import EditProductPage from '../ProductPage/EditProduct'
+import Navbar from '../../Components/Navbar'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
       <Routes>
         <Route path='/' element={<ProductPage/>} exact/>
         <Route path="/add-product" element={<CreateProductPage />} />
         <Route path="/product-info/:productID" element={<ProductInfoPage />} />
         <Route path="/edit-product/:productID" element={<EditProductPage />} />
-
       </Routes>
     </>
   )
