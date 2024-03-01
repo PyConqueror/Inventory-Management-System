f01fd92ebe511594df88054e47353b5519edaa09
Wan Aqim <wanaqim@gmail.com>
2024-03-01 11:21:47 +0800

    add login and signup form

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,27 @@
 import { NavLink, Link } from "react-router-dom"
 import { useNavigate, Navigate } from "react-router-dom";
+import * as userService from '../API Services/userServices'
 
-function Navbar() {
+function Navbar({user, setUser}) {
     const navigate = useNavigate();
 
-    // function handleLogOut() {
-    //     navigate('/')
-    //     userService.logOut();
-    //     setUser(null);
-    // }
-    
+    function handleLogOut() {
+        navigate('/')
+        userService.logOut();
+        setUser(null);
+    }
+    console.log(user)
     return (
         <nav>
-            {/* <div className="nav-section">
-                <img className="logo" src="/logo.svg" />
+            <div className="nav-section">
                 <h2>Welcome, <br/>{user.name}!</h2>
-            </div> */}
+            </div>
             <div className="nav-section page-links">
                 <NavLink to={"/"}><p>Inventories</p></NavLink>
             </div>  
-            {/* <div className="nav-section">
-                <Link onClick={handleLogOut}><img src="/logout.svg"/><p>Log out</p></Link>
-            </div> */}
+            <div className="nav-section">
+                <Link onClick={handleLogOut}><p>Log out</p></Link>
+            </div>
         </nav>
     )
 }
