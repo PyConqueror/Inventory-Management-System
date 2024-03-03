4e59cbf38d8da75c2fede7a1039168c0ccd2f0d8
Wan Aqim <wanaqim@gmail.com>
2024-03-03 11:43:28 +0800

    add route block for accountType guest for add and edit product"

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -7,27 +7,37 @@ import EditProductPage from '../ProductPage/EditProduct'
 import Navbar from '../../Components/Navbar'
 import AuthPage from '../AuthPage/AuthPage'
 import { getUser } from '../../API Services/userServices'
+import { Navigate } from 'react-router-dom'
 
 function App() {
-  const [user, setUser] = useState(getUser)
+  const [user, setUser] = useState(getUser);
 
   return (
     <main>
-    { user ?
-    <>
-    <Navbar user={user} setUser={setUser}/>
-      <Routes>
-        <Route path='/' element={<ProductPage/>} exact/>
-        <Route path="/add-product" element={<CreateProductPage />} />
-        <Route path="/product-info/:productID" element={<ProductInfoPage user={user} />} />
-        <Route path="/edit-product/:productID" element={<EditProductPage />} />
-      </Routes>
-    </>
-      :
-      <AuthPage setUser={setUser}/>
-      }
+      {user ? (
+        <>
+          <Navbar user={user} setUser={setUser} />
+          <Routes>
+            <Route path="/" element={<ProductPage user={user} />} />
+            <Route path="/product-info/:productID" element={<ProductInfoPage user={user} />} />
+            {user.accountType === 'admin' ? (
+              <Route path="/add-product" element={<CreateProductPage />} />
+            ) : (
+              <Route path="/add-product" element={<Navigate replace to="/" />} />
+            )}
+            {user.accountType === 'admin' ? (
+              <Route path="/edit-product/:productID" element={<EditProductPage />} />
+            ) : (
+              <Route path="/edit-product/:productID" element={<Navigate replace to="/" />} />
+            )}
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
     </main>
-  )
+  );
 }
+
 
 export default App
